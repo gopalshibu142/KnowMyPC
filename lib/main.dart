087d433d6ece9c0da2ebda3cdb3d9808ea64ddff
Gopal S <78960894+MaDGaMeR142@users.noreply.github.com>
2023-03-04 18:17:56 +0530

    v1.5.9

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:desktop_window/desktop_window.dart';
 
 import 'package:data_table_2/data_table_2.dart';
 import 'package:animated_background/animated_background.dart';
+import 'package:glassmorphism/glassmorphism.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -86,8 +87,10 @@ Map ramdetails = {
   'maxsize': '',
   'availsize': '',
   'nos': '',
+  'tno': '',
   'mfs': '',
   'freq': [],
+  'maxcap': '',
   'manufacturer': [],
   'sizeeach': []
 };
@@ -129,6 +132,10 @@ Future getDetails() async {
   //   // l[i] = (double.parse(l[i]) / 1073741824).toString();
   // });
   ramdetails['sizeeach'] = l;
+  result = await Process.run('cmd', ['/c', 'wmic Memphysical get MemoryDevices']);
+  ramdetails['tno'] = result.stdout.replaceAll('MemoryDevices', '').trim();
+  result = await Process.run('cmd', ['/c', 'wmic Memphysical get MaxCapacity ']);
+  ramdetails['maxcap'] = (int.parse(result.stdout.replaceAll('MaxCapacity', '').trim())/1048576).toStringAsFixed(0);
 }
 
 class KMP extends StatefulWidget {
@@ -159,94 +166,100 @@ class _KMPState extends State<KMP> {
       appBar: AppBar(
         title: Text("KnowYourPC"),
       ),
-      body:
-          
-                    SingleChildScrollView(
-                      child: Center(
-                        child: Column(
-                          
-                            children: [
-                              Text("\n\n\tProcessor\n\n", style: head),
-                             
-                                
-                                 
-                                   DataTable(
-                                    
-                                    border: TableBorder.all(), columns: [
-                                    DataColumn(label: Text("Term", style: thead)),
-                                    DataColumn(label: Text("Value", style: thead)),
-                                  ], rows: [
-                                    DataRow(cells: [
-                                      DataCell(Text('Name')),
-                                      DataCell(Text(processordetails['name'] ?? ''))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Cores')),
-                                      DataCell(Text(processordetails['cores'] ?? ''))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Base Clock')),
-                                      DataCell(Text(
-                                          (double.parse(processordetails['clock'] ?? '0') /
-                                                      1000)
-                                                  .toString() +
-                                              " GHz"))
-                                    ]),
-                                  ]),
-                              Text(
-                                "\n\n\tRAM\n\n",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                                    DataTable(border: TableBorder.all(), columns: [
-                                    DataColumn(label: Text("Term", style: thead)),
-                                    DataColumn(label: Text("Value", style: thead)),
-                                  ], rows: [
-                                    DataRow(cells: [
-                                      DataCell(Text('Total RAM')),
-                                      DataCell(Text(ramdetails['maxsize'].toString()))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Available RAM')),
-                                      DataCell(Text(ramdetails['availsize'].toString()))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Slots used')),
-                                      DataCell(Text(
-                                          ramdetails['manufacturer'].length.toString()))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Ram Size (Each Slot)')),
-                                      DataCell(Text((ramdetails['sizeeach']).toString()))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Speed(MHz)')),
-                                      DataCell(Text(ramdetails['freq'].toString()))
-                                    ]),
-                                    DataRow(cells: [
-                                      DataCell(Text('Manufacturer')),
-                                      DataCell(
-                                          Text((ramdetails['manufacturer']).toString()))
-                                    ]),
-                                  ]),
-                              
-                              
-                              Text(
-                                "\n\n\tGraphics\n\n",
-                                style: TextStyle(fontSize: 20),
-                              ),
-                            ],
-                          
-                                        
-                                      ),
-                      ),
-                    ),
-                    
-                  );
-          
-          
-          //)
-        
-    
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              Text("\n\n\tProcessor\n\n", style: head),
+              DataTable(border: TableBorder.all(), columns: [
+                DataColumn(label: Text("Term", style: thead)),
+                DataColumn(label: Text("Value", style: thead)),
+              ], rows: [
+                DataRow(cells: [
+                  DataCell(Text('Name')),
+                  DataCell(Text(processordetails['name'] ?? ''))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Cores')),
+                  DataCell(Text(processordetails['cores'] ?? ''))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Base Clock')),
+                  DataCell(Text(
+                      (double.parse(processordetails['clock'] ?? '0') / 1000)
+                              .toString() +
+                          " GHz"))
+                ]),
+              ]),
+              Text(
+                "\n\n\tRAM\n\n",
+                style: TextStyle(fontSize: 20),
+              ),
+              DataTable(border: TableBorder.all(), columns: [
+                DataColumn(label: Text("Term", style: thead)),
+                DataColumn(label: Text("Value", style: thead)),
+              ], rows: [
+                DataRow(cells: [
+                  DataCell(Text('Total RAM')),
+                  DataCell(Text(ramdetails['maxsize'].toString()))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Available RAM')),
+                  DataCell(Text(ramdetails['availsize'].toString()))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Slots used/total')),
+                  DataCell(Text(ramdetails['manufacturer'].length.toString()+'/'+ramdetails['tno']))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Ram Size (Each Slot)')),
+                  DataCell(Text((ramdetails['sizeeach']).toString()))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Speed(MHz)')),
+                  DataCell(Text(ramdetails['freq'].toString()))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Manufacturer')),
+                  DataCell(Text((ramdetails['manufacturer']).toString()))
+                ]),
+                 DataRow(cells: [
+                  DataCell(Text('Total Supported')),
+                  DataCell(Text((ramdetails['maxcap']).toString()+"GB"))
+                ]),
+              ]),
+              Text(
+                "\n\n\tGraphics\n\n",
+                style: TextStyle(fontSize: 20),
+              ),
+              DataTable(border: TableBorder.all(), columns: [
+                DataColumn(label: Text("Term", style: thead)),
+                DataColumn(label: Text("Value", style: thead)),
+              ], rows: [
+                DataRow(cells: [
+                  DataCell(Text('Available GPUs')),
+                  DataCell(Text(processordetails['name'] ?? ''))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Cores')),
+                  DataCell(Text(processordetails['cores'] ?? ''))
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('Base Clock')),
+                  DataCell(Text(
+                      (double.parse(processordetails['clock'] ?? '0') / 1000)
+                              .toString() +
+                          " GHz"))
+                ]),
+              ]),
+              SizedBox(height: 150,)
+            ],
+          ),
+        ),
+      ),
+    );
+
+    //)
   }
 }
 
@@ -258,70 +271,6 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> with TickerProviderStateMixin {
-  ThemeData darkTheme = ThemeData(
-    scrollbarTheme: ScrollbarThemeData(
-      radius: Radius.circular(100),
-      thumbColor: MaterialStateColor.resolveWith((states) => Colors.red),
-      trackColor: MaterialStateColor.resolveWith((states) => Colors.grey),
-    ),
-    brightness: Brightness.dark,
-    primaryColor: Colors.blueGrey[900],
-    accentColor: Colors.blueAccent,
-    scaffoldBackgroundColor: Colors.grey[900],
-    appBarTheme: AppBarTheme(
-      color: Colors.grey[900],
-      elevation: 0,
-      iconTheme: IconThemeData(color: Colors.white),
-      actionsIconTheme: IconThemeData(color: Colors.white),
-      textTheme: TextTheme(
-        headline6: TextStyle(
-          color: Colors.white,
-          fontSize: 20,
-          fontWeight: FontWeight.bold,
-        ),
-      ),
-    ),
-    textTheme: TextTheme(
-      headline1: TextStyle(
-        color: Colors.white,
-        fontSize: 32,
-        fontWeight: FontWeight.bold,
-      ),
-      headline6: TextStyle(
-        color: Colors.white,
-        fontSize: 18,
-        fontWeight: FontWeight.bold,
-      ),
-      bodyText1: TextStyle(
-        color: Colors.white,
-        fontSize: 16,
-      ),
-      bodyText2: TextStyle(
-        color: Colors.white70,
-        fontSize: 14,
-      ),
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      fillColor: Colors.grey[800],
-      filled: true,
-      hintStyle: TextStyle(
-        color: Colors.white54,
-      ),
-      labelStyle: TextStyle(
-        color: Colors.white,
-        fontWeight: FontWeight.bold,
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.grey),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Colors.blueAccent),
-        borderRadius: BorderRadius.circular(8),
-      ),
-    ),
-  );
-
   var plswait = '';
 
   @override
@@ -329,31 +278,63 @@ class _LandingState extends State<Landing> with TickerProviderStateMixin {
     return Scaffold(
       body: AnimatedBackground(
           child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text('Say goodbye to \'I don\'t know!'),
-                SizedBox(
-                  height: 20,
-                ),
-                OutlinedButton(
-                    onPressed: () async {
-                      setState(() {
-                        plswait = "hold tight";
-                      });
-                      getDetails().then((value) {
+            child: GlassmorphicContainer(
+              width: 250,
+              height: 300,
+              borderRadius: 20,
+              blur: 0.1,
+              padding: EdgeInsets.all(40),
+              alignment: Alignment.bottomCenter,
+              border: 2,
+              linearGradient: LinearGradient(
+                  tileMode: TileMode.mirror,
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFFffffff).withOpacity(0.1),
+                    Color(0xFFFFFFFF).withOpacity(0.05),
+                  ],
+                  stops: [
+                    0.1,
+                    1,
+                  ]),
+              borderGradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 255, 255, 255).withOpacity(0.5),
+                  Color((0xff000000)).withOpacity(0.5),
+                ],
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text('Say goodbye to \'I don\'t know!'),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  OutlinedButton(
+                      onPressed: () async {
                         setState(() {
-                          plswait = "";
+                          plswait = "hold tight";
                         });
-                        
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => KMP()));
-                      });
-                    },
-                    child: Text('Get to know your system inside out')),
-                Text(plswait)
-              ],
+                        getDetails().then((value) {
+                          setState(() {
+                            plswait = "";
+                          });
+
+                          Navigator.push(context,
+                              MaterialPageRoute(builder: (context) => KMP()));
+                        });
+                      },
+                      child: Text(
+                        'KnowMyPC',
+                        style: TextStyle(),
+                      )),
+                  Text(plswait)
+                ],
+              ),
             ),
           ),
           vsync: this,
